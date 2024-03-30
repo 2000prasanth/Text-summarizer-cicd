d760737d1a5b8b961c7d4eb4a494bb65800d3fe9
Prasanth KV <60725771+2000prasanth@users.noreply.github.com>
2024-03-30 13:48:53 +0530

    54

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ from textSummarizer.logging import logger
 logger.info("Welcom to our custom logging")
 
 from textSummarizer.pipeline.stage01_dataingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage01_dataingestion import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- textSummarizer/components/data_ingestion.py
+++ textSummarizer/components/data_ingestion.py
@@ -89,6 +89,7 @@ import zipfile
 from textSummarizer.logging import logger
 from textSummarizer.utils.common import*
 #request=urllib.urlretrieve()
+from textSummarizer.entity import DataIngestionConfig
 
 class ConfigurationManager:
     def __init__(
@@ -125,6 +126,36 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config 
+    
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+
+    
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
+
+        
+    
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
+    
 
 try:
     config = ConfigurationManager()
